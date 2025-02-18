a986f0959669cb0979a40bfbc91a7b1d9f26d89f
Edwin Carpio <twinsanity864@hotmail.com>
2025-02-18 14:41:10 -0600

    css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/components/Navbar";
-import "./globals.css";  // Asegúrate de que esta línea esté presente
+import "../styles/global.css" // Asegúrate de que esta línea esté presente
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
